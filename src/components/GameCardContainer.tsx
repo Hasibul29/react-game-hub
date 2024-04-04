fc10c7b5ec1fed913be5febddae4df34abd16fce
Sheikh Md. Hasibul Hasan <skhasibul.c17@gmail.com>
2024-04-04 16:12:03 +0600

    display genre

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,7 @@ interface Prop {
 
 const GameCardContainer = ({ children }: Prop) => {
   return (
-    <Box width="300px" borderRadius="10px" overflow="hidden">
+    <Box borderRadius="10px" overflow="hidden">
       {children}
     </Box>
   );
