c145030f8f91c6234e30d2efb39a91d942a0608a
Sheikh Md. Hasibul Hasan <skhasibul.c17@gmail.com>
2024-04-07 14:18:19 +0600

    refactoring

--- src/hooks/UseGame.ts
+++ src/hooks/UseGame.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData, { FetchResponse } from "./useData";
+import { FetchResponse } from "../services/api-client";
 import apiClient from "../services/api-client";
 
 export interface Platform {
