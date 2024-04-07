e906725231308bd66f0a312723da620dd3000c29
Sheikh Md. Hasibul Hasan <skhasibul.c17@gmail.com>
2024-04-07 15:16:42 +0600

    infinite query

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,16 +1,24 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { Button, SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGame";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 import { GameQuery } from "../App";
+import React from "react";
 
 interface Prop {
   gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: Prop) => {
-  const { data, error, isLoading } = useGames(gameQuery);
+  const {
+    data,
+    error,
+    isLoading,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7];
 
   if (error) return <Text>{error.message}</Text>;
@@ -26,9 +34,18 @@ const GameGrid = ({ gameQuery }: Prop) => {
             <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      {data?.results.map((game) => (
-        <GameCard key={game.id} game={game} />
+      {data?.pages.map((page, index) => (
+        <React.Fragment key={index}>
+          {page.results.map((game) => (
+            <GameCard key={game.id} game={game} />
+          ))}
+        </React.Fragment>
       ))}
+      {hasNextPage && (
+        <Button onClick={() => fetchNextPage()}>
+          {isFetchingNextPage ? "Loading...." : "Next"}
+        </Button>
+      )}
     </SimpleGrid>
   );
 };
